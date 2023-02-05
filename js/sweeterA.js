ada53452ff29e7d57b84086f32b23c2797907185
Jonathan Peña <97184743+JJPENA666@users.noreply.github.com>
2023-02-04 23:11:14 -0500

    listo cambios

---
+++ js/sweeterA.js
@@ -0,0 +1,9 @@
+
+Swal.fire({
+  title: 'Gracias!',
+  text: 'Llena el formulario para contactarnos.',
+  imageUrl: 'https://www.metroecuador.com.ec/resizer/jW_B19qhKPCLa7aJNBVHlgNhutk=/800x0/filters:format(jpg):quality(70)/cloudfront-us-east-1.images.arcpublishing.com/metroworldnews/XQ4JITDFFZGOTLXO564DM7TM2E.jpg',
+  imageWidth: 400,
+  imageHeight: 200,
+  imageAlt: 'Custom image',
+})
